5040c9f101d75c3d87f40019c154bc4a090ccbf2
Pedro Martins <pedromartinsdelemos@gmail.com>
2025-09-15 20:34:03 -0300

    Working with Route Parameters To Pass Data Between Screens

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import { FlatList } from 'react-native';
-import CategoryGridTile from '../components/CategoryGridTile';
-import { CATEGORIES } from '../data/dummy-data'; // seu array de categorias
+import React from "react";
+import { FlatList } from "react-native";
+import CategoryGridTile from "../components/CategoryGridTile";
+import { CATEGORIES } from "../data/dummy-data"; // seu array de categorias
 
-function CategoriesScreen() {
+function CategoriesScreen({ navigation }) {
   function renderCategoryItem(itemData) {
+    function pressHandler() {
+        navigation.navigate("MealsOverview", {
+            categoryId: itemData.item.id,
+            
+        });
+    }
+
     return (
-      <CategoryGridTile 
-        title={itemData.item.title} 
-        color={itemData.item.color} 
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
       />
     );
   }
 
   return (
-    <FlatList 
-      data={CATEGORIES} 
-      renderItem={renderCategoryItem} 
-      keyExtractor={(item) => item.id} 
-      numColumns={2} 
+    <FlatList
+      data={CATEGORIES}
+      renderItem={renderCategoryItem}
+      keyExtractor={(item) => item.id}
+      numColumns={2}
     />
   );
 }
