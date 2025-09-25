952eb9cd545562a0682377c579e968389dcdde98
Pedro Martins <pedromartinsdelemos@gmail.com>
2025-09-25 14:25:21 -0300

    Managing App-wide State with Context

--- App.js
+++ App.js
@@ -7,51 +7,54 @@ import MealDetailScreen from './screens/MealDetailScreen';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import FavoriteScreen from './screens/FavoritesScreen';
 import { Ionicons } from "@expo/vector-icons";
+import FavoritesContextProvider from './store/context/favorites-context';
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 
 function DrawerNavigator() {
   return <Drawer.Navigator screenOptions={{
-    headerStyle: {backgroundColor: '#351401'},
-            headerTintColor: 'white',
-            sceneContainerStyle: {backgroundColor: '#3f2f25'},
-            drawerContentStyle: {backgroundColor: '#351401'},
-            drawerActiveTintColor: "white",
-            drawerActiveBackgroundColor: "#9c7660ff"
+    headerStyle: { backgroundColor: '#351401' },
+    headerTintColor: 'white',
+    sceneContainerStyle: { backgroundColor: '#3f2f25' },
+    drawerContentStyle: { backgroundColor: '#351401' },
+    drawerActiveTintColor: "white",
+    drawerActiveBackgroundColor: "#9c7660ff"
   }}>
     <Drawer.Screen name="Categories" component={CategoriesScreen} options={{
       title: 'All Categories',
-      drawerIcon: ({ color, size}) => 
+      drawerIcon: ({ color, size }) =>
         <Ionicons name="list" color={color} size={size} />
-    }}/>
+    }} />
     <Drawer.Screen name="Favorites" component={FavoriteScreen} options={{
-      drawerIcon: ({ color, size}) => 
+      drawerIcon: ({ color, size }) =>
         <Ionicons name="star" color={color} size={size} />
-    }}/>
+    }} />
   </Drawer.Navigator>
 }
 
 export default function App() {
   return (
     <>
-      <StatusBar style='light'/>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{
-          headerStyle: {backgroundColor: '#351401'},
+      <StatusBar style='light' />
+      <FavoritesContextProvider>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{
+            headerStyle: { backgroundColor: '#351401' },
             headerTintColor: 'white',
-            contentStyle: {backgroundColor: '#3f2f25'}
-        }}>
-          <Stack.Screen name="Drawer" component={DrawerNavigator} 
-          options={{
-            headerShown: false
-          }}/>
-          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} 
-          />
-          <Stack.Screen name="MealDetail" component={MealDetailScreen} 
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+            contentStyle: { backgroundColor: '#3f2f25' }
+          }}>
+            <Stack.Screen name="Drawer" component={DrawerNavigator}
+              options={{
+                headerShown: false
+              }} />
+            <Stack.Screen name="MealsOverview" component={MealsOverviewScreen}
+            />
+            <Stack.Screen name="MealDetail" component={MealDetailScreen}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </FavoritesContextProvider>
     </>
   );
 }
---
+++ store/context/favorites-context.js
@@ -0,0 +1,32 @@
+import { createContext } from "react";
+
+export const FavoritesContext = createContext({
+    ids: [],
+    addFavorite: (id) => {},
+    removeFavorite: (id) => {}
+});
+
+export default function FavoritesContextProvider({children}) {
+    const [favoriteMealIDs, setFavoriteMealIds] = useState([]);
+
+    function addFavorite(id) {
+        setFavoriteMealIds((currentFavIds) => [...currentFavIds, id]);
+    }
+
+    function removeFavorite(id) {
+        setFavoriteMealIds((currentFavIds) => currentFavIds.filter(mealId => mealId !== id));
+    }
+
+    //passing these function into context provider
+
+    const value = {
+        ids: favoriteMealIDs,
+        addFavorite: addFavorite,
+        removeFavorite: removeFavorite
+    };
+
+    return <FavoritesContext.Provider value={value}>
+        {children}
+    </FavoritesContext.Provider>
+}
+
